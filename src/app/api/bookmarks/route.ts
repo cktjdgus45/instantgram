3c9b78e0a85b16e85e3d048cd1c73343de1596dd
pooh <cktjdgus45@naver.com>
2023-05-06 21:39:19 +0900

    add bookmark server

---
+++ src/app/api/bookmarks/route.ts
@@ -0,0 +1,20 @@
+import { getServerSession } from 'next-auth';
+import { NextRequest, NextResponse } from 'next/server';
+import { authOptions } from '../../../../pages/api/auth/[...nextauth]';
+import { addBookmark, removeBookmark } from '@/service/user';
+
+export async function PUT(req: NextRequest) {
+    const session = await getServerSession(authOptions);
+    const user = session?.user;
+    if (!user) {
+        return new Response('Authenticate Error', { status: 401 });
+    }
+    const { id, bookmark } = await req.json();
+    if (!id || bookmark === undefined) {
+        return new Response('Bad Request', { status: 400 });
+    }
+    const request = bookmark ? addBookmark : removeBookmark;
+    return request(id, user.id)
+        .then(res => NextResponse.json(res))
+        .catch((error) => new Response(JSON.stringify(error), { status: 500 }))
+}
